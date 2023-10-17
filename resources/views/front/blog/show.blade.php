5ccf8c1fcd3e678ac345775a0eaedcd621ec7821
Edwin Roquet <roquet_ing@hotmail.com>
2023-10-17 14:33:44 -0600

    se agrego style.css

--- resources/views/front/blog/show.blade.php
+++ resources/views/front/blog/show.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 
 <body>
--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
+   
 {{-- 
     <script>
         window.addEventListener("pageshow", function(event) {
